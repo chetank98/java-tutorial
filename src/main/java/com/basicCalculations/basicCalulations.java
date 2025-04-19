394463c98ae8c113a0d636de586f9ad0ac7a5d04
Chetan Kumar <chetan-mac@Chetans-MacBook-Air.local>
2025-04-19 14:48:05 +0530

    basic caulation program

---
+++ src/main/java/com/basicCalculations/basicCalulations.java
@@ -0,0 +1,32 @@
+package com.basicCalculations;
+
+public class basicCalulations {
+
+    public static void main(String[] args) {
+
+        int x=20;
+        int y=15;
+
+        int sum=x+y;
+
+        int diff=x-y;
+
+        int product = x*y;
+
+        int quotient=x/y;
+
+        int remainder=x%y;
+
+        System.out.println("Sum of x and y is: "+sum);
+
+        System.out.println("Difference of x and y is: "+diff);
+
+        System.out.println("Product of x and y is: "+product);
+
+        System.out.println("Quotient of x and y is: "+quotient);
+
+        System.out.println("Remainder of x and y is: "+remainder);
+
+    }
+
+}
